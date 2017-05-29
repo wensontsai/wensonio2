1071f97f7ef1c4b2031e38445fc77e110f841245
Wenson Tsai <wensontsai@gmail.com>
2017-05-29 12:58:09 -0400

    sass fixes

--- src/routes/Counter/components/Counter.js
+++ src/routes/Counter/components/Counter.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import EndlessPlanet from '../assets/endless_planet.mp4'
+import EndlessPlanetVid from '../assets/endless_planet.mp4'
+
 import './Counter.scss'
 
 export const Counter = ({ counter, increment, doubleAsync }) => (
     <div className='pageCenter'>
-        <video muted autoPlay id='bgvid' src={EndlessPlanet} loop>
+        <video muted autoPlay id='bgvid' src={EndlessPlanetVid} loop>
         </video>
         <div className='main'>
             <div className='bizcard'>
@@ -35,13 +36,15 @@ export const Counter = ({ counter, increment, doubleAsync }) => (
                             innovation and craftsmanship.
                         </p>
                         <p>Previously, he worked in the music industry processing licenses and deals
-                            for all the major players: Spotify, Apple, Google, major music labels, et al.
+                            for all the major players: Google, Apple, Spotify, major music labels, et al.
                             The software he wrote turned data into royalty checks for everyone whose music
                             you have ever enjoyed.
                         </p>
-                        <p>Presently, he writes the <a href='https://www.makerbot.com/print/'
-                            target='blank'>desktop app</a> for 3D-printing dreams to life,
-                            while pushing model-slicing and toolpathing algorithms towards industry innovation.
+                        <p>Presently, he works to push model-slicing and toolpathing algorithims toward industry
+                            innovation at MakerBot Industries.  He also writes the <a href='https://www.makerbot.com/print/'
+                            target='blank'>desktop app</a> for 3D-printing dreams to life.
+                        </p>
+                        <p>Please follow his experiments <a href='http://hackers.wenson.io' target='blank'>here</a>.
                         </p>
                         <p>In another life, he was a <a href='https://www.flickr.com/photos/itswenson/'
                             target='blank'>photographer</a> who traveled and exhibited internationally.
